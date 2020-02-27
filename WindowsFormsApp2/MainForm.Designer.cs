ae89a8789b2c105c84de7c57142145bbe5bd212d
Nikita Korabelnikov <34415162+visuable@users.noreply.github.com>
2020-02-27 21:53:37 +0300

    Initial commit

---
+++ WindowsFormsApp2/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenSecondFormButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenSecondFormButton
+            // 
+            this.OpenSecondFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenSecondFormButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenSecondFormButton.Name = "OpenSecondFormButton";
+            this.OpenSecondFormButton.Size = new System.Drawing.Size(410, 387);
+            this.OpenSecondFormButton.TabIndex = 0;
+            this.OpenSecondFormButton.Text = "Открыть вторую форму";
+            this.OpenSecondFormButton.UseVisualStyleBackColor = true;
+            this.OpenSecondFormButton.Click += new System.EventHandler(this.OpenSecondFormButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.OpenSecondFormButton);
+            this.Name = "MainForm";
+            this.Text = "Главная форма";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenSecondFormButton;
+    }
+}
+
---
+++ WindowsFormsApp2/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class MainForm : Form
+    {
+        // Создаем экземпляр класса второй формы.
+        SecondForm form = new SecondForm();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSecondFormButton_Click(Object sender, EventArgs e)
+        {
+            // Далее необходимо "привязать" метод-обработчик события.
+            form.WordListBox.SelectedIndexChanged += WordListBox_SelectedIndexChanged;
+            // Тогда все необходимые действия будут происходить именно в первой форме!
+            // Далее открываем вторую форму в виде дочернего окна.
+            form.ShowDialog(this);
+        }
+
+        private void WordListBox_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            // "((ListBox) sender)" называется явным приведением и необходимо, поскольку метод-обработчик находится в коде первой форме, а не во второй.
+            // Переменная "sender" вызывается с таким типом, каким вызвало событие этого элемента. Например: если была нажата кнопка, то тип данных "sender" - Button.
+            // В данном случае вызов осуществляется событием элемента ListBox, поэтому "sender" имеет тип ListBox. В противном случае, если явное преобразование будет неправильным, приложение выдаст исключение(ошибку).
+            MessageBox.Show(((ListBox)sender).Text, "Вызов из первой формы!");
+        }
+    }
+}
